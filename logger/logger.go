dcedddf6888d038dab7c95bebf85be9038978ad2
Jeff Alyanak <jeff@alyanak.ca>
2020-02-22 06:42:49 -0500

    Added error check and made golint happy

--- logger/logger.go
+++ logger/logger.go
@@ -1,30 +1,37 @@
 package logger
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"sync"
 )
 
-type logger struct {
+// Logger struct bundles log.Logger and filename
+type Logger struct {
 	filename string
 	*log.Logger
 }
 
-var lg *logger
+var lg *Logger
 var once sync.Once
 
-func Get() *logger {
+// Get Returns a Logger
+func Get() *Logger { // TODO: Add user configurable log directory
 	once.Do(func() {
-		lg = makeLogger("godaddy-check.log")
+		lg = makeLogger("check_godaddy.log")
 	})
 	return lg
 }
 
-func makeLogger(fname string) *logger {
-	file, _ := os.OpenFile(fname, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
+func makeLogger(fname string) *Logger {
+	file, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
+	if err != nil {
+		fmt.Println("Could not open log file at: ", fname)
+		os.Exit(3)
+	}
 
-	return &logger{
+	return &Logger{
 		filename: fname,
 		Logger:   log.New(file, "", log.Ldate|log.Ltime),
 	}
